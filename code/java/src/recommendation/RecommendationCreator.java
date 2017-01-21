939e07c0039ba7e245bfd4077c4ffa1cfe816c13
ahui21 <alwin.iwillwin@gmail.com>
2017-01-20 23:49:00 -0800

    added get and set methods in recommendationcreator

--- code/java/src/recommendation/RecommendationCreator.java
+++ code/java/src/recommendation/RecommendationCreator.java
@@ -1,5 +1,45 @@
 package recommendation;
 
-public class RecommendationCreator {
+import java.util.*;
 
+public class RecommendationCreator {
+	private WeightedRankings weightedRankings;
+	private Recommendation[] recommendations;
+	
+	public RecommendationCreator() {
+		this.weightedRankings = new WeightedRankings();
+		this.recommendations = new Recommendation[32];
+	}
+	
+	public void print() {
+		this.sort();
+		
+		for (int i = 0; i < this.recommendations.length; i++) {
+			
+		}
+	}
+	
+	public void setWeightedRankings(WeightedRankings weightedRankings) {
+		this.weightedRankings = weightedRankings;
+	}
+	
+	public void setRecommendations(Recommendation[] recommendations) {
+		this.recommendations = recommendations;
+	}
+	
+	public WeightedRankings getWeightedRankings() {
+		return this.weightedRankings;
+	}
+	
+	public Recommendation[] getRecommendations() {
+		return this.recommendations;
+	}
+	
+	public void sort() {
+		
+	}
+	
+	public Recommendation[] getRecommendations(int numberOfRecommendations) {
+		return Arrays.copyOfRange(this.recommendations, 0, numberOfRecommendations);
+	}
 }
